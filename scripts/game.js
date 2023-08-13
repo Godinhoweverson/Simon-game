5701a695765326abf37be4c78c722545db0c2363
Godinhoweverson <>
2023-08-13 09:27:02 +0000

    add showTurns fucntion()

--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,46 @@
 let game = {
-    score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: ["button1", "button2", "button3", "button4"],
-
+    score: 0,
+    turnNumber: 0,
+    choices: ["button1", "button2", "button3", "button4"]
 };
 
-function newGame(){
-    game.score = 0;
+function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
+    game.score = 0;
+    showScore();
+    addTurn();
 }
 
-module.exports = { game, newGame};
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if(game.turnNumber >= game.currentGame.lenght){
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
